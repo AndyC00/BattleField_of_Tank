dc7f57789c2c52f4aaba5b1d1796e17389b530af
AndyC00 <chenjinzhi96@gmail.com>
2024-08-02 15:46:25 +1200

    1

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+#include <SDL.h>
+
+// Local includes: 
+#include "game.h" 
+#include "logmanager.h"
+
+int main(int argc, char* argv[]) 
+{
+    Game& gameInstance = Game::GetInstance(); 
+
+    if (!gameInstance.Initialise())
+    {
+        LogManager::GetInstance().Log("Game initialise failed!");
+        return 1;
+    }
+
+    while (gameInstance.DoGameLoop())
+    {
+        // No body.
+    }
+
+    Game::DestroyInstance(); 
+    LogManager::DestroyInstance();
+
+    return 0;
+
+}
